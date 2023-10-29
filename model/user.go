8f8cfdb6dfd7b9e99b3a02e28f06e92811253309
KU4NG <1214966109@qq.com>
2023-10-29 21:47:57 +0800

    user 模型优化

--- model/user.go
+++ model/user.go
@@ -4,36 +4,33 @@ import "github.com/golang-module/carbon/v2"
 
 // 用户模型
 type User struct {
-    Id       uint            `gorm:"primaryKey;comment:自增编号" json:"id"`
-    Name     string          `gorm:"not null;comment:姓名" json:"name"`
-    Landline string          `gorm:"comment:固话号码" json:"landline"`
-    Mobile   string          `gorm:"uniqueIndex:uidx_mobile;comment:手机号" json:"mobile"`
-    Email    string          `gorm:"uniqueIndex:uidx_email;comment:邮箱" json:"email"`
-    Password string          `gorm:"not null;comment:密码" json:"-"` // json 中不显示 password 字段
-    JobId    string          `gorm:"uniqueIndex:uidx_job_id;comment:工号" json:"job_id"`
-    JobName  string          `gorm:"not null;comment:岗位名称" json:"job_name"`
-    JoinTime carbon.DateTime `gorm:"comment:入职日期" json:"join_time"`
-    // 关联部门
-    DepartmentId uint       `gorm:"comment:部门id" json:"department_id"`
-    Department   Department `gorm:"foreignKey:DepartmentId;comment:部门" json:"department"`
-    // 关联省市区
-    OfficeProvinceId uint            `gorm:"comment:省id" json:"office_province_id"`
-    OfficeProvince   Region          `gorm:"foreignKey:OfficeProvinceId;comment:省" json:"office_province"`
-    OfficeCityId     uint            `gorm:"comment:市id" json:"office_city_id"`
-    OfficeCity       Region          `gorm:"foreignKey:OfficeCityId;comment:市" json:"office_city"`
-    OfficeDistrictId uint            `gorm:"comment:区id" json:"office_district_id"`
-    OfficeDistrict   Region          `gorm:"foreignKey:OfficeDistrictId;comment:区" json:"office_district"`
-    OfficeAddress    string          `gorm:"comment:详细地址" json:"office_address"`
-    OfficeStation    string          `gorm:"comment:工位" json:"office_station"`
-    NativeProvinceId uint            `gorm:"comment:省id" json:"native_province_id"`
-    NativeProvince   Region          `gorm:"foreignKey:NativeProvinceId;comment:省" json:"native_province"`
-    NativeCityId     uint            `gorm:"comment:市id" json:"native_city_id"`
-    NativeCity       Region          `gorm:"foreignKey:NativeCityId;comment:市" json:"native_city"`
-    Gender           *uint           `gorm:"type:tinyint(1);default:1;comment:性别(1=男,2=女,3=未知)" json:"gender"`
-    Avatar           string          `gorm:"comment:头像" json:"avatar"`
-    Birthday         carbon.DateTime `gorm:"comment:生日" json:"birthday"`
-    // 关联用户自身
-    CreatorId          uint            `gorm:"comment:创建人id" json:"creator_id"`
+    Id                 uint            `gorm:"primaryKey;comment:自增编号" json:"id"`
+    Name               string          `gorm:"not null;comment:姓名" json:"name"`
+    Landline           string          `gorm:"comment:固话号码" json:"landline"`
+    Mobile             string          `gorm:"uniqueIndex:uidx_mobile;comment:手机号" json:"mobile"`
+    Email              string          `gorm:"uniqueIndex:uidx_email;comment:邮箱" json:"email"`
+    Password           string          `gorm:"not null;comment:密码" json:"-"` // json 中不显示 password 字段
+    JobId              string          `gorm:"uniqueIndex:uidx_job_id;comment:工号" json:"job_id"`
+    JobName            string          `gorm:"not null;comment:岗位名称" json:"job_name"`
+    JoinTime           carbon.DateTime `gorm:"comment:入职日期" json:"join_time"`
+    DepartmentId       uint            `gorm:"comment:部门id" json:"department_id"` // 关联部门
+    Department         Department      `gorm:"foreignKey:DepartmentId;comment:部门" json:"department"`
+    OfficeProvinceId   uint            `gorm:"comment:办公地点省id" json:"office_province_id"` // 关联省市区
+    OfficeProvince     Region          `gorm:"foreignKey:OfficeProvinceId;comment:省" json:"office_province"`
+    OfficeCityId       uint            `gorm:"comment:办公地点市id" json:"office_city_id"`
+    OfficeCity         Region          `gorm:"foreignKey:OfficeCityId;comment:市" json:"office_city"`
+    OfficeDistrictId   uint            `gorm:"comment:办公地点区id" json:"office_district_id"`
+    OfficeDistrict     Region          `gorm:"foreignKey:OfficeDistrictId;comment:区" json:"office_district"`
+    OfficeAddress      string          `gorm:"comment:办公地点详细地址" json:"office_address"`
+    OfficeStation      string          `gorm:"comment:办公地点工位" json:"office_station"`
+    NativeProvinceId   uint            `gorm:"comment:籍贯省id" json:"native_province_id"`
+    NativeProvince     Region          `gorm:"foreignKey:NativeProvinceId;comment:省" json:"native_province"`
+    NativeCityId       uint            `gorm:"comment:籍贯市id" json:"native_city_id"`
+    NativeCity         Region          `gorm:"foreignKey:NativeCityId;comment:市" json:"native_city"`
+    Gender             *uint           `gorm:"type:tinyint(1);default:1;comment:性别(1=男,2=女,3=未知)" json:"gender"`
+    Avatar             string          `gorm:"comment:头像" json:"avatar"`
+    Birthday           carbon.DateTime `gorm:"comment:生日" json:"birthday"`
+    CreatorId          uint            `gorm:"comment:创建人id" json:"creator_id"` // 关联用户自身
     Creator            *User           `gorm:"foreignKey:CreatorId;-" json:"creator"`
     FirstLogin         *uint           `gorm:"type:tinyint(1);default:1;comment:是否第一次登录(0=否,1=是)" json:"first_login"`
     LastLogin          carbon.DateTime `gorm:"comment:最后一次登录时间" json:"last_login"`
@@ -41,10 +38,9 @@ type User struct {
     LastChangePassword carbon.DateTime `gorm:"comment:最后一次修改密码时间" json:"last_change_password"`
     WrongTimes         uint            `gorm:"comment:登录错误次数" json:"wrong_times"`
     Status             *uint           `gorm:"type:tinyint(1);default:1;comment:用户状态(0=禁用,1=正常,2=未激活,3=锁定)" json:"status"`
-    // 关联角色
-    RoleId    uint `gorm:"comment:角色id" json:"role_id"`
-    Role      Role `gorm:"foreignKey:RoleId;comment:角色" json:"role"`
-    BaseModel      // 基础字段信息
+    RoleId             uint            `gorm:"comment:角色id" json:"role_id"` // 关联角色
+    Role               Role            `gorm:"foreignKey:RoleId;comment:角色" json:"role"`
+    BaseModel                          // 基础字段信息
 }
 
 // 自定义表名
